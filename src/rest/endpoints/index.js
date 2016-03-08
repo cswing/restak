9ae9090b8ef27deda3edc33fae3e336654454e16
Craig Swing <cswing@gmail.com>
2016-03-08 12:30:48 -0500

    Make ResourcePutEndpoint available

--- src/rest/endpoints/index.js
+++ src/rest/endpoints/index.js
@@ -17,3 +17,4 @@ module.exports.Endpoint = require('./endpoint');
 module.exports.CollectionEndpoint = require('./collection-endpoint');
 module.exports.ResourceEndpoint = require('./resource-endpoint');
 module.exports.ResourceQueryEndpoint = require('./resource-query-endpoint');
+module.exports.ResourcePutEndpoint = require('./resource-put-endpoint');
