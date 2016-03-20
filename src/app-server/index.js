a9e0d1cf66ab9d04c4cd8a7328d8c340190919aa
Craig Swing <cswing@gmail.com>
2016-03-20 09:14:23 -0400

    Clean up docs and module exports

---
+++ src/app-server/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * The app-server namespace contains the classes that are necessaary to configure and run an application.
+ * The core classes are the {@link restak.app-server.ApplicationContext} and {@link restak.app-server.ApplicationServer}.
+ *
+ * @namespace restak.app-server
+ */
+
+/**
+ * Describes the basics about an application.
+ *
+ * @typedef ApplicationDescriptor
+ * @memberof restak.app-server
+ * @type {object}
+ * @property {string} name - The name of the application.
+ * @property {string} version - The version of the application.
+ */
+
+/**
+ * A registrar function that registers objects with an {@link restak.app-server.ApplciationContext|application context}.
+ *
+ * @function register
+ * @memberof restak.app-server
+ * @param {restak.app-server.ApplicationContext} appContext - The application context.
+ */
+
+module.exports.ApplicationContext = require('./application-context');
+module.exports.ApplicationServer = require('./application-server');
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 'use strict';
 
+module.exports.appServer = require('./app-server');
 module.exports.query = require('./query');
 module.exports.rest = require('./rest');
+//module.exports.scheduler = require('./scheduler');
