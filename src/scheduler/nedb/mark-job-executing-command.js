f97d8c73ea1eff9989a02266ff9969cb95432d29
Craig Swing <cswing@gmail.com>
2016-03-24 17:26:35 -0400

    Implement mark-job-executing-command

---
+++ src/scheduler/nedb/mark-job-executing-command.js
@@ -0,0 +1,114 @@
+'use strict';
+
+var log4js = require('log4js'),
+	logger = log4js.getLogger('restak.scheduler.nedb.MarkJobExecutingCommand'),
+	async = require('async'),
+	models = require('../models'),
+	JobDescriptorStatus = models.JobDescriptorStatus,
+	JobInstanceStatus = models.JobInstanceStatus;
+
+/**
+ * Mark a job and job instance as executing and persist to the file system.
+ *
+ * @constructor
+ * @implements restak.commands.Command
+ * @memberof restak.scheduler.nedb
+ * @param {nedb.Datastore} jobStore - The NeDB datastore for jobs.
+ * @param {nedb.Datastore} jobInstanceStore - The NeDB datastore for job instances.
+ * @param {restak.util.ObjectTransform} jobTransform - optional, a way to transform the job from what exists in the store to what should be returned.
+ * @param {restak.util.ObjectTransform} jobInstanceTransform - optional, a way to transform the instance from what exists in the store to what should be returned.
+ */
+var MarkJobExecutingCommand = function(jobStore, jobInstanceStore, jobTransform, jobInstanceTransform){
+	
+	/**
+	 * The datastore that contains jobs.
+	 *
+	 * @protected
+	 * @type nedb.Datastore
+	 */
+	this.jobStore = jobStore;
+
+	/**
+	 * The datastore that contains job instances.
+	 *
+	 * @protected
+	 * @type nedb.Datastore
+	 */
+	this.jobInstanceStore = jobInstanceStore;
+
+	/**
+	 * A transform that will modify what is returned from the datastore before beeing returned to the caller.
+	 *
+	 * @protected
+	 * @type restak.util.ObjectTransform
+	 */
+	this.jobTransform = jobTransform;
+
+	/**
+	 * A transform that will modify what is returned from the datastore before beeing returned to the caller.
+	 *
+	 * @protected
+	 * @type restak.util.ObjectTransform
+	 */
+	this.jobInstanceTransform = jobInstanceTransform;
+};
+
+/** @inheritdoc */
+MarkJobExecutingCommand.prototype.execute = function(cmdInstr, callback){
+
+	var jobStore = this.jobStore,
+		jobInstanceStore = this.jobInstanceStore,
+		data = cmdInstr.data,
+		job = data.job,
+		jobInstance = data.instance,
+		jobTransform = this.jobTransform,
+		jobInstanceTransform = this.jobInstanceTransform;
+
+	var query = { _id: job.id },
+		update = {
+			$set: { 
+				status: JobDescriptorStatus.Executing
+			}
+		};
+
+	jobStore.update(query, update, function (err, numReplaced) {
+		if(err) {
+			return callback(err, null);
+		}
+
+		if(numReplaced == 0) {
+			return callback('A job with the id does not exist: ' + job.id, null);
+		}
+
+		jobInstance.status = JobInstanceStatus.Executing;
+		jobInstanceStore.insert(jobInstance, function (err, instance) {
+			
+			if(err) {
+				return callback(err, null);
+			}
+
+			if(jobInstanceTransform) {
+				instance = jobInstanceTransform.transform(instance);
+			}
+
+			jobStore.find(query, function(err, docs){
+			
+				var job = docs[0];
+
+				if(jobTransform) {
+					job = jobTransform.transform(job);
+				}
+
+				logger.debug('Job instance [' + instance.instanceId + '] created for ' + job.name + ' [' + job.id + ']');
+
+				callback(null, {
+					job: job,
+					instance: instance
+				});
+			});
+		});
+
+	});
+};
+
+module.exports = MarkJobExecutingCommand;
---
+++ src/scheduler/nedb/tests/mark-job-executing-command-tests.js
@@ -0,0 +1,112 @@
+'use strict';
+
+var expect = require('chai').expect,
+	assert = require('chai').assert,
+	mock = require('mock-fs'),
+	moment = require('moment'),
+	Datastore = require('nedb'),
+	models = require('../../models'),
+	transforms = require('../transforms'),
+	JobDescriptorStatus = models.JobDescriptorStatus,
+	JobInstanceStatus = models.JobInstanceStatus,
+	MarkJobExecutingCommand = require('../mark-job-executing-command');
+
+var testJob = { "id": "12345", "name": "testJob", "data": {} };
+
+describe('scheduler > nedb > mark-job-executing', function() {
+
+	describe('#execute', function(){
+
+		it('should update the job', function(done) {
+
+			var jobStore = new Datastore(),
+				jobInstanceStore = new Datastore(),
+				command = new MarkJobExecutingCommand(jobStore, jobInstanceStore, transforms.jobTransform, transforms.jobInstanceTransform);
+
+			jobStore.insert([{
+					"_id": "12345",
+					"name": "Test Job"
+				}], function (err, newDocs) {
+					var instr = {
+						data: {
+							job: {
+								id: testJob.id,
+								name: testJob.name,
+								status: null
+							},
+							instance: {
+								jobId: testJob.id,
+								name: testJob.name,
+								instanceId: null,
+								status: null,
+								user: {
+									id: 'SYSTEM',
+									name: 'System'
+								},
+								data: testJob.data
+							}
+						}
+					};
+
+					command.execute(instr, function(err, result){
+						expect(err).to.be.null;
+						
+						expect(result).to.have.deep.property('job.status', JobDescriptorStatus.Executing);
+						expect(result).to.have.deep.property('instance.instanceId');
+						expect(result).to.have.deep.property('instance.status', JobInstanceStatus.Executing);
+						
+						jobStore.find({ _id: testJob.id }, function(err, jobs){
+							if(err) 
+								return done(err);
+
+							var job = jobs[0];
+
+							expect(job).to.have.property('status', JobDescriptorStatus.Executing);
+
+							jobInstanceStore.find({_id: result.instance.instanceId}, function(err, instances){
+								if(err) 
+									return done(err);
+
+								var instance = instances[0];
+
+								expect(instance).to.have.property('status', JobInstanceStatus.Executing);
+								done();
+							});
+						});
+					});
+				}); // jobStore.insert
+		});
+
+		it('it should err if the job does not exist', function(done) {
+
+			var jobStore = new Datastore(),
+				jobInstanceStore = new Datastore(),
+				command = new MarkJobExecutingCommand(jobStore, jobInstanceStore, transforms.jobTransform, transforms.jobInstanceTransform),
+				instr = {
+					data: {
+						job: {
+							id: testJob.id,
+							name: testJob.name,
+							status: null
+						},
+						instance: {
+							jobId: testJob.id,
+							name: testJob.name,
+							instanceId: null,
+							status: null,
+							user: {
+								id: 'SYSTEM',
+								name: 'System'
+							},
+							data: testJob.data
+						}
+					}
+				};
+
+			command.execute(instr, function(err, result){
+				expect(err).to.equal('A job with the id does not exist: 12345');
+				done();
+			});
+		});
+	});
+});
