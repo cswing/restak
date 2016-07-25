4166bf7bc638ef533fd49d3b44e549deda1a09bd
Craig Swing <cswing@gmail.com>
2016-07-25 11:07:25 -0400

    Provide extension to register a job

---
+++ src/job-scheduler/application-context-extensions.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var log4js = global.log4js || require('log4js'),
+	logger = log4js.getLogger('restak.nedb'),
+	ApplicationContext = require('../app-server/application-context');
+
+
+ApplicationContext.prototype.registerJob = function(key, jobCommand, configProperty){
+	
+	var name = configProperty.name,
+		description = configProperty.description;
+
+	this.registerCommand(key, jobCommand);
+
+	var data = {
+		commandKey: key,
+		name: name,
+		description: description
+	};
+
+	this.registerDeferredExecution('restak.scheduler.InstallJobCommand', 'Install job: ' + name, data);
+};
+
+module.exports = ApplicationContext;
---
+++ src/job-scheduler/tests/application-context-extensions-tests.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var util = require('util'),
+	expect = require('chai').expect,
+	assert = require('chai').assert,
+	ApplicationContext = require('../application-context-extensions'),
+	DefaultConfig = require('../../app-server').DefaultConfig;
+
+describe('scheduler > application-context', function() {
+
+	var config = new DefaultConfig({
+		'appName': 'Custom app name',
+		'appVersion': '0.1.0-TEST'
+	});
+
+	describe('#registerJob', function(){
+
+		it('should register the job', function(done) {
+
+			var command = {
+					execute: function(){}
+				},
+				appContext = new ApplicationContext(config);
+
+			appContext.registerJob('test.job', command, {
+				name: 'Test Job',
+				description: 'Test Job Description'
+			});
+
+			expect(appContext.getCommand('test.job')).to.equal(command);
+			expect(appContext.deferreds).to.have.deep.members([{
+				commandKey: 'restak.scheduler.InstallJobCommand',
+				description: 'Install job: Test Job',
+				data: {
+					commandKey: 'test.job',
+					name: 'Test Job',
+					description: 'Test Job Description'
+				}				
+			}]);
+
+			done();
+		});
+
+	});
+});
