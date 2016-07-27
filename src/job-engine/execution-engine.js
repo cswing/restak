b90c97862cbc6af7fca539a9b1075f75e08c6350
Craig Swing <cswing@gmail.com>
2016-07-27 09:12:28 -0400

    Implement new execution-engine for jobs

---
+++ src/job-engine/execution-engine.js
@@ -0,0 +1,120 @@
+'use strict';
+
+var log4js = global.log4js || require('log4js'),
+	logger = log4js.getLogger('restak.job-engine.execution-engine'),
+	async = require('async'),
+	models = require('./models'),
+	JobInstanceStatus = models.JobInstanceStatus;
+
+var ExecutionEngine = function(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor){
+
+	this.instanceQuery = instanceQuery;
+
+	this.markInstanceExecutingCommand = markInstanceExecutingCommand;
+
+	this.markInstanceExecutedCommand = markInstanceExecutedCommand;
+
+	this.commandExecutor = commandExecutor;
+};
+
+ExecutionEngine.prototype.executeJob = function(instance, callback){
+
+	var _t = this,
+		commandExecutor = this.commandExecutor;
+
+	try {
+
+		commandExecutor._execute(instance.commandKey, instance.params || null, callback);
+
+	} catch(err){
+		
+		var err = err || 'An error occurred';
+		
+		if(err instanceof Error) {
+			callback(err.message, null);
+		} else {
+			callback((err || '').toString(), null);
+		}
+	}	
+};
+
+ExecutionEngine.prototype.execute = function(callback){
+
+	var _t = this,
+		instanceQuery = this.instanceQuery,
+		markInstanceExecutingCommand = this.markInstanceExecutingCommand,
+		markInstanceExecutedCommand = this.markInstanceExecutedCommand,
+		queryRequest = {
+			pageSize: 100,
+			filter: 'status=\'' + JobInstanceStatus.Queued + '\''
+		};
+
+	instanceQuery.execute(queryRequest, function(err, result){
+
+		if(err){
+			var message = 'Unable to find queued jobs: ' + err; 
+			logger.error(message);
+			
+			return callback(message);
+		}
+
+		logger.debug('Found ' + result.items.length + ' queued jobs.');
+
+		async.map(result.items, function(instance, cb){
+
+			var instr = {
+				data: {
+					instanceId: instance.id
+				}
+			};
+
+			markInstanceExecutingCommand.execute(instr, function(err, updatedInstance){
+				if(err){
+					logger.error('An error occurred preparing for job execution [instance: ' + instance.id + ']: ' + err);
+					cb(null, null); // an error here should not prevent other jobs from executing
+				}
+
+				cb(null, updatedInstance);
+			});
+
+		}, function(err, jobsToExecute) {
+			
+			if(err){ // should never be true
+				logger.error('An error occurred preparing jobs for execution: ' + err);
+				return;
+			}
+
+			var tasks = jobsToExecute.filter(function(instance){
+				return instance !== null;
+			}).map(function(instance){
+				return function(cb){
+					_t.executeJob(instance, function(err, result){
+
+						var instr = {
+							data: {
+								instanceId: instance.id,
+								result: err || result
+							}
+						};
+
+						markInstanceExecutedCommand.execute(instr, function(err, result){
+							if(err){
+								logger.error('An error occurred preparing for job execution [instance: ' + instance.id + ']: ' + err);
+							}
+
+							cb();
+						});
+					});
+				};
+			});
+
+			async.parallel(tasks, function(err){
+				logger.debug('Execution complete for ' + result.items.length + ' queued jobs.');
+				callback(null);
+			});
+
+		}); // async.map
+	}); // instanceQuery.execute
+};
+
+module.exports = ExecutionEngine;
---
+++ src/job-engine/tests/execution-engine-tests.js
@@ -0,0 +1,349 @@
+'use strict';
+
+var util = require('util'),
+	expect = require('chai').expect,
+	assert = require('chai').assert,
+	models = require('../models'),
+	JobInstanceStatus = models.JobInstanceStatus,
+	ExecutionEngine = require('../execution-engine'),
+	CommandExecutorMock = require('./command-executor-mock');
+
+describe('job-engine > execution-engine', function() {
+
+	describe('#execute', function(){
+
+		it('should handle no queued jobs', function(done){
+
+			var queryRequest = null,
+				instanceQuery = {
+					execute: function(qr, callback){
+						queryRequest = qr;
+						callback(null, {
+							items: []
+						});
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, null, null, null);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(queryRequest).to.deep.equal({
+						pageSize: 100,
+						filter: 'status=\'QUEUED\''
+					});
+					done();
+				});
+		});
+
+		it('should handle when the query calls back with an error', function(done){
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback('An error occurred', null);
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, null, null, null);
+
+				engine.execute(function(err){
+					expect(err).to.equal('Unable to find queued jobs: An error occurred');
+					done();
+				});
+		});
+
+		it('should execute the jobs returned from the query', function(done){
+
+			var instances = {
+				'1': {
+					id: '1',
+					name: 'test job a',
+					commandKey: 'test.jobA'
+				},
+				'2': {
+					id: '2',
+					name: 'test job b',
+					commandKey: 'test.jobB'
+				},
+				'3': {
+					id: '3',
+					name: 'test job c',
+					commandKey: 'test.jobC'					
+				}
+			};
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback(null, {
+							items: [
+								instances['1'],
+								instances['2'],
+								instances['3']
+							]
+						});
+					}
+				},
+				markInstanceExecutingCommandData = [],
+				markInstanceExecutingCommand = {
+					execute: function(instr, callback){
+						markInstanceExecutingCommandData.push(instr.data);
+						callback(null, instances[instr.data.instanceId]);
+					}
+				}, 
+				markInstanceExecutedCommandData = [],
+				markInstanceExecutedCommand = {
+					execute: function(instr, callback){
+						markInstanceExecutedCommandData.push(instr.data);
+						callback(null, instances[instr.data.instanceId]);	
+					}
+				},
+				commandExecutorData = [],
+				commandExecutor = {
+					_execute: function(key, data, callback){
+						commandExecutorData.push({
+							key: key,
+							data: data
+						});
+						callback(null, null);
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(markInstanceExecutingCommandData).to.have.deep.members([
+							{ instanceId: '1' },
+							{ instanceId: '2' },
+							{ instanceId: '3' }
+						]);
+					expect(commandExecutorData).to.have.deep.members([
+							{ 
+								key: 'test.jobA',
+								data: null
+							},
+							{ 
+								key: 'test.jobB',
+								data: null
+							},
+							{ 
+								key: 'test.jobC',
+								data: null
+							}
+						]);
+					expect(markInstanceExecutedCommandData).to.have.deep.members([
+							{ instanceId: '1', result: null },
+							{ instanceId: '2', result: null },
+							{ instanceId: '3', result: null }
+						]);
+
+					done();
+				});
+		});
+
+		it('should pass the params from the instance to the command', function(done){
+
+			var instance = {
+				id: 'A',
+				name: 'test job a',
+				commandKey: 'test.jobA',
+				params: {
+					arg1: 'a',
+					arg2: 'b'
+				}
+			};
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback(null, {
+							items: [ instance ]
+						});
+					}
+				},
+				markInstanceExecutingCommand = {
+					execute: function(instr, callback){
+						callback(null, instance);
+					}
+				}, 
+				markInstanceExecutedCommand = {
+					execute: function(instr, callback){
+						callback(null, instance);
+					}
+				},
+				commandExecutorData = [],
+				commandExecutor = {
+					_execute: function(key, data, callback){
+						commandExecutorData.push({
+							key: key,
+							data: data
+						});
+						callback(null, null);
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(commandExecutorData).to.have.deep.members([
+							{ 
+								key: 'test.jobA',
+								data: {
+									arg1: 'a',
+									arg2: 'b'
+								}
+							}
+						]);
+					
+					done();
+				});
+		});
+
+		it('should set the result on the instance when the command succeeds', function(done){
+
+			var instance = {
+				id: 'A',
+				name: 'test job a',
+				commandKey: 'test.jobA'
+			};
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback(null, {
+							items: [ instance ]
+						});
+					}
+				},
+				markInstanceExecutingCommand = {
+					execute: function(instr, callback){
+						callback(null, instance);
+					}
+				}, 
+				markInstanceExecutedCommandData = [],
+				markInstanceExecutedCommand = {
+					execute: function(instr, callback){
+						markInstanceExecutedCommandData.push(instr.data);
+						callback(null, instance);
+					}
+				},
+				commandExecutor = {
+					_execute: function(key, data, callback){
+						callback(null, {
+							arg1: 'a',
+							arg2: 'b'
+						});
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(markInstanceExecutedCommandData).to.have.deep.members([
+							{ 
+								instanceId: 'A',
+								result: {
+									arg1: 'a',
+									arg2: 'b'
+								}
+							}
+						]);
+					
+					done();
+				});
+		});
+
+		it('should set the error on the instance when the command calls back with an error', function(done){
+
+			var instance = {
+				id: 'A',
+				name: 'test job a',
+				commandKey: 'test.jobA'
+			};
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback(null, {
+							items: [ instance ]
+						});
+					}
+				},
+				markInstanceExecutingCommand = {
+					execute: function(instr, callback){
+						callback(null, instance);
+					}
+				}, 
+				markInstanceExecutedCommandData = [],
+				markInstanceExecutedCommand = {
+					execute: function(instr, callback){
+						markInstanceExecutedCommandData.push(instr.data);
+						callback(null, instance);
+					}
+				},
+				commandExecutor = {
+					_execute: function(key, data, callback){
+						callback('An error occurred', {
+							arg1: 'a',
+							arg2: 'b'
+						});
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(markInstanceExecutedCommandData).to.have.deep.members([
+							{ 
+								instanceId: 'A',
+								result: 'An error occurred'
+							}
+						]);
+					
+					done();
+				});
+		});
+
+		it('should handle when the command throws an unexpected exception', function(done){
+
+			var instance = {
+				id: 'A',
+				name: 'test job a',
+				commandKey: 'test.jobA'
+			};
+
+			var instanceQuery = {
+					execute: function(qr, callback){
+						callback(null, {
+							items: [ instance ]
+						});
+					}
+				},
+				markInstanceExecutingCommand = {
+					execute: function(instr, callback){
+						callback(null, instance);
+					}
+				}, 
+				markInstanceExecutedCommandData = [],
+				markInstanceExecutedCommand = {
+					execute: function(instr, callback){
+						markInstanceExecutedCommandData.push(instr.data);
+						callback(null, instance);
+					}
+				},
+				commandExecutor = {
+					_execute: function(key, data, callback){
+						throw 'An uncaught error';
+					}
+				},
+				engine = new ExecutionEngine(instanceQuery, markInstanceExecutingCommand, markInstanceExecutedCommand, commandExecutor);
+
+				engine.execute(function(err){
+					expect(err).to.be.null;
+					expect(markInstanceExecutedCommandData).to.have.deep.members([
+							{ 
+								instanceId: 'A',
+								result: 'An uncaught error'
+							}
+						]);
+					
+					done();
+				});
+		});
+
+	});
+});
